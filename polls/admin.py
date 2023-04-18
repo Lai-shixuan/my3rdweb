5a580784b2485fc978f2ad3ed8afbea458d4882a
laisholdwin10 <laishixuan123@gmail.com>
2023-04-18 15:27:27 +0800

    进行了一些bug修正工作，并且设置了admin，用户名laish，并且将question对象加入了admin中

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+
+from .models import Question
+
+admin.site.register(Question)   # 大概需要2个import去在admin中注册这个对象
