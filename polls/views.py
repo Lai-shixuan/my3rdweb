0826bee107a8c464fc78df086c48e113f4a43948
laisholdwin10 <laishixuan123@gmail.com>
2023-04-19 20:00:52 +0800

    add 1 httpresponse in view

--- polls/views.py
+++ polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -18,3 +19,8 @@ def results(request, question_id):
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
+
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
