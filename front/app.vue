3f3d503fc1d0ced19a6fdc2591ef1502ed968361
laisholdwin10 <laishixuan123@gmail.com>
2023-04-20 21:48:11 +0800

    new name

---
+++ front/app.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+    // 组件选项
+    // 此处声明一些响应式状态
+    data(){
+        return{
+            message:'Hello World!'
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Make me dynamic! {{message}}</h1>
+</template>
