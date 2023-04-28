da6aeb15a95958b39593ed34a73c0bc1b7329d71
laisholdwin10 <laishixuan123@gmail.com>
2023-04-28 13:32:00 +0800

    my first java program

--- Java&Algorithms/java/src/Main.java
+++ Java&Algorithms/java/src/Main.java
@@ -1,10 +1,16 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
+    public static int larger(int x, int y){
+        if (x > y){
+            return x;
+        }
+        return y;
+    }
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.printf("Hello and welcome!\n");
 
         // Press Shift+F10 or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
@@ -13,5 +19,6 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+        System.out.println(larger(-5, 8));
     }
 }
