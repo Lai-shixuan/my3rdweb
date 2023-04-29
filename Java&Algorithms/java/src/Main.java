c4e7aac83e4f34f03dbd1697e20457323a037bf5
laisholdwin10 <laishixuan123@gmail.com>
2023-04-29 15:11:14 +0800

    basic Java practice

--- Java&Algorithms/java/src/Main.java
+++ Java&Algorithms/java/src/Main.java
@@ -1,24 +1,22 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static int larger(int x, int y){
-        if (x > y){
-            return x;
-        }
-        return y;
+    public  static int cut4pieces(double x, double y){
+       if (x == 0 || y == 0)
+           return 0;
+       else if (x > 0) {
+           if (y > 0)
+               return 1;
+           else return 4;
+       }else if (y > 0)
+           return 2;
+       else return 3;
     }
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!\n");
-
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
-        System.out.println(larger(-5, 8));
+        double x = 24.3;
+        double y = -22;
+        System.out.println(cut4pieces(x, y));
     }
 }
