b7e7d2b7ecf9b0364618b227ddbbab4920f66a39
laisholdwin10 <laishixuan123@gmail.com>
2023-05-01 19:12:46 +0800

    some css

--- vue3-md-blog/src/views/Post.vue
+++ vue3-md-blog/src/views/Post.vue
@@ -1,12 +1,12 @@
 <template>
-  <PatchMeta :title="title" />
-  <div class="container my-4 my-md-5">
-    <v-md-editor mode="preview" v-model="markDownSource" :style="`background-color: ${VUE_APP_MAIN_BG_CSS_COLOR}; color: ${VUE_APP_MAIN_TEXT_CSS_COLOR};`"></v-md-editor>
-    <button type="button" :style="`color: ${VUE_APP_MAIN_TEXT_CSS_COLOR};`" class="border btn mt-4"
-      @click="hasHistory() ? router.go(-1) : router.push('/')">
-      &laquo; Back
-    </button>
-  </div>
+    <PatchMeta :title="title" />
+    <div class="container my-4 my-md-5">
+        <v-md-editor mode="preview" v-model="markDownSource" :style="`background-color: ${VUE_APP_MAIN_BG_CSS_COLOR}; color: ${VUE_APP_MAIN_TEXT_CSS_COLOR};`"></v-md-editor>
+        <button type="button" :style="`color: ${VUE_APP_MAIN_TEXT_CSS_COLOR};`" class="border btn mt-4"
+                @click="hasHistory() ? router.go(-1) : router.push('/')">
+            &laquo; Back
+        </button>
+    </div>
 </template>
 
 <script setup lang="ts">
@@ -21,15 +21,15 @@ import blogConfig from '../blog_config'
 const { VUE_APP_MAIN_BG_CSS_COLOR, VUE_APP_MAIN_TEXT_CSS_COLOR } = blogConfig
 
 const props = defineProps({
-  id: {
-    type: String,
-    default: ''
-  }
+    id: {
+        type: String,
+        default: ''
+    }
 })
 
 /* Hacky navigation when a href link is clicked within the compiled html Post */
 onBeforeRouteUpdate(() => {
-  location.reload()
+    location.reload()
 })
 
 // Fetch Post markdown and compile it to html
