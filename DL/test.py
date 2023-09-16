3d7148e45bfb37890cd4508213d9411942efbd13
laisholdwin10 <laishixuan123@gmail.com>
2023-09-16 19:08:59 +0800

    long long ago

--- DL/test.py
+++ DL/test.py
@@ -1,10 +1,9 @@
-import torch
+#%%
 
-x = torch.randn(9, 3)
-y = x.repeat(1, 2)
-z = y.view(9, 2, 3)
-zx = z.permute(1, 0, 2)
-zy = zx[0, :]
-zz = zx[0]
-zzx = zz[:-1]
-# y = x[0, :]
+for i in range(10):
+    print(i)
+
+list1 = []
+list1.append(5)
+list1.append(6)
+print(list1[1])
