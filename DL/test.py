17ca94017c687728d86d9bae7666b9c492da7750
laisholdwin10 <laishixuan123@gmail.com>
2023-10-15 19:44:06 +0800

    HW03

--- DL/test.py
+++ DL/test.py
@@ -1,9 +1,24 @@
 #%%
+import torch
+import numpy as np
+import pandas
+from PIL import Image, ImageFilter
+import matplotlib
 
-for i in range(10):
-    print(i)
+#%%
+torch.cuda.is_available()
 
-list1 = []
-list1.append(5)
-list1.append(6)
-print(list1[1])
+print(np.random.random([1, 2]))
+
+print(torch.cuda.is_available())
+print(torch.cuda.current_device())
+print(torch.cuda.get_device_name(0))
+print(torch.cuda.device_count())
+
+print(torch.__version__)
+
+#%%
+im1 = Image.open(r"../image/download24.jpg")
+im1.show()
+im2 = im1.filter(ImageFilter.MedianFilter(size=9))
+im2.show()
