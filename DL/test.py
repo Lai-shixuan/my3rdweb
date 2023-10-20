8a0ee845bfc2b7542779c78ec3c6169da202630e
laisholdwin10 <laishixuan123@gmail.com>
2023-10-20 14:03:52 +0800

    the first version, autoaugment, probably wrong

--- DL/test.py
+++ DL/test.py
@@ -22,3 +22,27 @@ im1 = Image.open(r"../image/download24.jpg")
 im1.show()
 im2 = im1.filter(ImageFilter.MedianFilter(size=9))
 im2.show()
+
+
+#%%
+#import numpy as np
+import torch
+import os
+import torch.nn as nn
+import torchvision.transforms as transforms
+from PIL import Image
+# "ConcatDataset" and "Subset" are possibly useful when doing semi-supervised learning.
+from torch.utils.data import ConcatDataset, DataLoader, Subset, Dataset
+from torchvision.datasets import DatasetFolder, VisionDataset
+
+# This is for the progress bar.
+from tqdm.auto import tqdm
+import random
+#%%
+myseed = 6666  # set a random seed for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(myseed)
