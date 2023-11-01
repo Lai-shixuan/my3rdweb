45222f20f8e4a7cf017b148ede9c4da164f4ba7e
laisholdwin10 <laishixuan123@gmail.com>
2023-11-01 20:55:16 +0800

    0.02R^2

--- DL/a.py
+++ DL/a.py
@@ -1,5 +1,8 @@
-import test
+import torch
+from torcheval.metrics import R2Score
 
-def print_abc():
-    print('a')
-
+metric = R2Score()
+input = torch.tensor([1.2, 2.5, 3.6, 4.5, 6])
+target = torch.tensor([1, 2, 3, 4, -5])
+metric.update(input, target)
+a = metric.compute()
