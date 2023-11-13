21780dd154d0aac9c0af6130371f9ff073209980
laisholdwin10 <laishixuan123@gmail.com>
2023-11-13 22:46:59 +0800

    archievd

--- DL/test.py
+++ DL/test.py
@@ -1,48 +1,84 @@
 #%%
 import torch
-import numpy as np
-import pandas
-from PIL import Image, ImageFilter
-import matplotlib
+from torch import nn
+from torch.utils.data import DataLoader, TensorDataset
+from torch.optim import Adam
+from sklearn.model_selection import KFold
+from sklearn.datasets import make_classification
 
 #%%
-torch.cuda.is_available()
+# 定义一个简单的二分类模型
+class SimpleClassifier(nn.Module):
+    def __init__(self):
+        super(SimpleClassifier, self).__init__()
+        self.fc = nn.Linear(20, 2)
 
-print(np.random.random([1, 2]))
-
-print(torch.cuda.is_available())
-print(torch.cuda.current_device())
-print(torch.cuda.get_device_name(0))
-print(torch.cuda.device_count())
-
-print(torch.__version__)
+    def forward(self, x):
+        return self.fc(x)
 
 #%%
-im1 = Image.open(r"../image/download24.jpg")
-im1.show()
-im2 = im1.filter(ImageFilter.MedianFilter(size=9))
-im2.show()
+# 创建一个模拟数据集
+X, y = make_classification(n_samples=100, n_features=20, n_informative=20, n_redundant=0, random_state=42)
+X = torch.tensor(X, dtype=torch.float32)
+y = torch.tensor(y, dtype=torch.long)
 
+# 定义损失函数
+criterion = nn.CrossEntropyLoss()
+
+# 5-fold cross validation
+kfold = KFold(n_splits=5, shuffle=True, random_state=42)
 
 #%%
-#import numpy as np
-import torch
-import os
-import torch.nn as nn
-import torchvision.transforms as transforms
-from PIL import Image
-# "ConcatDataset" and "Subset" are possibly useful when doing semi-supervised learning.
-from torch.utils.data import ConcatDataset, DataLoader, Subset, Dataset
-from torchvision.datasets import DatasetFolder, VisionDataset
 
-# This is for the progress bar.
-from tqdm.auto import tqdm
-import random
+for i in enumerate(kfold.split(X)):
+    print(i)
+
 #%%
-myseed = 6666  # set a random seed for reproducibility
-torch.backends.cudnn.deterministic = True
-torch.backends.cudnn.benchmark = False
-np.random.seed(myseed)
-torch.manual_seed(myseed)
-if torch.cuda.is_available():
-    torch.cuda.manual_seed_all(myseed)
+for fold, (train_ids, test_ids) in enumerate(kfold.split(X)):
+    print(f'FOLD {fold}')
+    print('--------------------------------')
+
+    # 定义模型和优化器
+    model = SimpleClassifier()
+    optimizer = Adam(model.parameters(), lr=0.001)
+
+    # 划分训练集和验证集
+    X_train, X_test = X[train_ids], X[test_ids]
+    y_train, y_test = y[train_ids], y[test_ids]
+
+    # 创建 DataLoader
+    train_dataset = TensorDataset(X_train, y_train)
+    test_dataset = TensorDataset(X_test, y_test)
+    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=16)
+
+    # 模型训练
+    for epoch in range(10):  # 在实际应用中，你可能需要更多的epoch
+        for inputs, targets in train_loader:
+            optimizer.zero_grad()
+
+            # 前向传播
+            outputs = model(inputs)
+            loss = criterion(outputs, targets)
+
+            # 后向传播和优化
+            loss.backward()
+            optimizer.step()
+
+        # 输出loss
+        if (epoch+1) % 5 == 0:
+            print (f'Epoch [{epoch+1}/10], Loss: {loss.item()}')
+
+    # 验证模型
+    model.eval()
+    total = 0
+    correct = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    accuracy = correct / total
+    print(f'Accuracy of the model on the test set: {accuracy * 100} %')
