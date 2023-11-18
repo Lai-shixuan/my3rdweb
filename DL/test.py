5e420f9481320b610a0649def2a1fea36f96600e
laisholdwin10 <laishixuan123@gmail.com>
2023-11-18 13:29:18 +0800

    many changes

--- DL/test.py
+++ DL/test.py
@@ -1,84 +1,44 @@
-#%%
-import torch
-from torch import nn
-from torch.utils.data import DataLoader, TensorDataset
-from torch.optim import Adam
-from sklearn.model_selection import KFold
-from sklearn.datasets import make_classification
+from PIL import Image
+import numpy as np
+import sklearn
 
-#%%
-# 定义一个简单的二分类模型
-class SimpleClassifier(nn.Module):
-    def __init__(self):
-        super(SimpleClassifier, self).__init__()
-        self.fc = nn.Linear(20, 2)
+def calculate_histogram(image):
+    histogram = np.zeros(256)
+    for pixel in image:
+        histogram[pixel] += 1
+    return histogram / histogram.sum()
 
-    def forward(self, x):
-        return self.fc(x)
+def otsu_threshold(histogram):
+    total = 1
+    current_max, threshold = 0, 0
+    sumT, sumF, sumB = 0, 0, 0
+    for i in range(0, 256):
+        sumT += i * histogram[i]
+    weightB = 0
+    for i in range(0, 256):
+        weightB += histogram[i]
+        weightF = total - weightB
+        if weightB == 0 or weightF == 0:
+            continue
+        sumB += i * histogram[i]
+        sumF = sumT - sumB
+        mB = sumB / weightB
+        mF = sumF / weightF
+        varBetween = weightB * weightF * (mB - mF) ** 2
+        if varBetween > current_max:
+            current_max = varBetween
+            threshold = i
+    return threshold
 
-#%%
-# 创建一个模拟数据集
-X, y = make_classification(n_samples=100, n_features=20, n_informative=20, n_redundant=0, random_state=42)
-X = torch.tensor(X, dtype=torch.float32)
-y = torch.tensor(y, dtype=torch.long)
+def apply_threshold(image, threshold):
+    return image.point(lambda p: p > threshold and 255)
 
-# 定义损失函数
-criterion = nn.CrossEntropyLoss()
+def otsu_method(image_path):
+    image = Image.open(image_path).convert('L')
+    histogram = calculate_histogram(image.getdata())
+    threshold = otsu_threshold(histogram)
+    return apply_threshold(image, threshold)
 
-# 5-fold cross validation
-kfold = KFold(n_splits=5, shuffle=True, random_state=42)
-
-#%%
-
-for i in enumerate(kfold.split(X)):
-    print(i)
-
-#%%
-for fold, (train_ids, test_ids) in enumerate(kfold.split(X)):
-    print(f'FOLD {fold}')
-    print('--------------------------------')
-
-    # 定义模型和优化器
-    model = SimpleClassifier()
-    optimizer = Adam(model.parameters(), lr=0.001)
-
-    # 划分训练集和验证集
-    X_train, X_test = X[train_ids], X[test_ids]
-    y_train, y_test = y[train_ids], y[test_ids]
-
-    # 创建 DataLoader
-    train_dataset = TensorDataset(X_train, y_train)
-    test_dataset = TensorDataset(X_test, y_test)
-    train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=16)
-
-    # 模型训练
-    for epoch in range(10):  # 在实际应用中，你可能需要更多的epoch
-        for inputs, targets in train_loader:
-            optimizer.zero_grad()
-
-            # 前向传播
-            outputs = model(inputs)
-            loss = criterion(outputs, targets)
-
-            # 后向传播和优化
-            loss.backward()
-            optimizer.step()
-
-        # 输出loss
-        if (epoch+1) % 5 == 0:
-            print (f'Epoch [{epoch+1}/10], Loss: {loss.item()}')
-
-    # 验证模型
-    model.eval()
-    total = 0
-    correct = 0
-    with torch.no_grad():
-        for inputs, labels in test_loader:
-            outputs = model(inputs)
-            _, predicted = torch.max(outputs, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    accuracy = correct / total
-    print(f'Accuracy of the model on the test set: {accuracy * 100} %')
+# 使用这个函数
+binary_image = otsu_method('d:\GoogleDrive\\04 microCT土壤\\20231026 2次组会\\30μm-ROI36mm\q1.1 (315).bmp')
+binary_image.show()
